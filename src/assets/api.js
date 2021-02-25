e6d7ae26bd9f34fa8be454f56ed65cb1e52b706a
KinXpeng <kinxpeng@163.com>
2021-02-25 17:20:16 +0800

    提交

---
+++ src/assets/api.js
@@ -0,0 +1,9 @@
+const ApiUrl = "https://www.kinxpeng.com/demo/js/jyw/imgs/";
+
+module.exports = {
+  navUrl: ApiUrl,
+  indexStatic: ApiUrl + "index",
+  nowStatic: ApiUrl + "now",
+  shopStatic: ApiUrl + "shop",
+  mineStatic: ApiUrl + "mine",
+};
---
+++ src/components/intro.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="intro"></div>
+</template>
+
+<script>
+import dataJson from "@/assets/jyw.json";
+export default {
+  data() {
+    return {
+      src: null,
+    };
+  },
+  created() {
+    let url = "https://www.kinxpeng.com/demo/js/jyw/imgs/";
+    let res = dataJson.data;
+    console.log(res.nav);
+  },
+};
+</script>
+
+<style></style>
